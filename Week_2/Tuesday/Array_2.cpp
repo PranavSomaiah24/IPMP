7384aa43ac4ec63e89b4d4454cd5d6d584999570
Pranav Somaiah <63319858+PranavSomaiah24@users.noreply.github.com>
2021-04-11 19:20:24 +0300

    Delete Array_2.cpp

--- Week_2/Tuesday/Array_2.cpp
+++
@@ -1,9 +0,0 @@
-// Mising number in range 1-N
-
-int MissingNumber(vector<int>& array, int n) {
-    int totalSum=n*(n+1)/2,sum=0;
-    for(int i=0;i<n-1;i++)
-        sum+=array[i];
-        
-    return (totalSum-sum);
-}
